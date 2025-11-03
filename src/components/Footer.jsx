283218e0ded2a0daa3e62e36754e067b6ac423c6
Janette033 <janetterautiainen@hotmail.com>
2025-11-03 18:48:33 +0200

    Add hero background & fix image imports

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
       <a href="mailto:janetterautiainen@hotmail.com?subject=Yhteydenotto">Sähköposti</a>
       <a href="https://github.com/Janette033">GitHub</a>
       <a href="http://www.linkedin.com/in/janette-rautiainen-403956335">LinkedIn</a>
-      <p>© 2025 My Portfolio</p>
+      <p>© 2025 Janette Rautiainen</p>
     </footer>
   );
 }
